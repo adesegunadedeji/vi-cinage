1580837367f6f93ec278108ac794359d6fc99a53
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-08-30 17:05:59 -0400

    Container

---
+++ vi-cinage-frontend/src/container/realEstateContainer.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class realEstateContainer extends Component{
+    constructor(){
+        super();
+        this.state={    
+            
+        }
+    }
+
+
+    render(){
+        return(
+            <div>
+                <h1> Employee Database</h1>
+            </div>
+        )
+    }
+}
+export default realEstateContainer
