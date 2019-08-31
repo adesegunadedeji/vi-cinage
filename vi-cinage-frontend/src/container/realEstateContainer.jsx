99245cd3373e239983c9fb09503bc7aa65fa9565
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-08-30 23:20:48 -0400

    working on RealEstate List

--- vi-cinage-frontend/src/container/realEstateContainer.jsx
+++ vi-cinage-frontend/src/container/realEstateContainer.jsx
@@ -4,10 +4,27 @@ class realEstateContainer extends Component{
     constructor(){
         super();
         this.state={    
-            
+            realEstate:[]
         }
     }
 
+        getEstates = async() => {
+            try{
+            const realEstate = await fetch("http://localhost:9000/api/v1/realEstate")
+            const parsedResponse = await realEstate.json();
+            console.log(parsedResponse);
+                if(parsedResponse.code === 200){
+                    this.setState({
+                        realEstate: parsedResponse.data
+                    })
+                    console.log(this.state.realEstate)
+                }
+            }
+            catch(err){
+                console.log(err);
+            }
+
+        }
 
     render(){
         return(
